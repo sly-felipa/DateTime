8e7b7d5d56c301a561912f25771b2b1c882a88c2
sly-felipa <sfelipa@cifpfbmoll.eu>
2021-01-17 23:49:12 +0100

    Initial commit

---
+++ app/src/main/java/com/example/datetime/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.datetime;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    Button buttonDate;
+    Button buttonTime;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        buttonDate = findViewById(R.id.buttonDate);
+        buttonTime = findViewById(R.id.buttonTime);
+    }
+
+    public void showTimePickerDialog(View v) {
+        DialogFragment newFragment = new TimePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "timePicker");
+    }
+
+    public void showDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
+}
